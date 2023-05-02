21ae29ea27b6e61d4dffdb583a59c6f61c890e5a
Christian Barbosa <christian.barbosa05222001@gmail.com>
2023-05-02 22:13:05 +0800

    updated arduino code

--- finalCapstoneCode.ino
+++ finalCapstoneCode.ino
@@ -1,12 +1,13 @@
 //library here
 #include <DHT.h>
 #include <Servo.h>
+#include <LiquidCrystal_I2C.h>   
 Servo myservoWindow;
 //declare pins of the following sensors
 const int fan = 4; //fan pin to pin 4
 const int humiditySensor = 2; //Dht22 to pin 2
 const int rainSensor = A1; //rainsensor to A1
-const int servoMotor = 12;//Servo motor to pin 12
+//const int servoMotor = 12;//Servo motor to pin 12
 const int pump = A7; //pump to pin 7
 const int photoresistor = A0; //photoresistor to A0
 #define DHTTYPE DHT22   // DHT 11 or DHT 22
@@ -18,12 +19,15 @@ int humidityVal;
 int photoVal;
 int rainVal;
 
+
+LiquidCrystal_I2C lcd(0x27, 16, 2); 
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(fan, OUTPUT);
   pinMode(humiditySensor, INPUT);
   pinMode(rainSensor, INPUT);
-  pinMode(servoMotor, 12);
+  //pinMode(servoMotor, 12);
   pinMode(pump, OUTPUT);
   pinMode(photoVal, INPUT);
   myservoWindow.attach(9);
@@ -31,6 +35,8 @@ void setup() {
   Serial.begin(9600);
 
   dht.begin();
+  lcd.init();                          // initialize the LCD
+  lcd.backlight();   
 
   //digitalWrite(pump, HIGH);
   //delay(10000);
@@ -64,7 +70,7 @@ void readDHT(){
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
 
-  if(temperature >= 15){
+  if(temperature >= 15){ //if temperature is equal or greater than 15
     digitalWrite(fan, HIGH);
     myservoWindow.write(180);
   }else{
@@ -83,5 +89,17 @@ void readDHT(){
   Serial.print("Temperature: ");
   Serial.print(temperature);
   Serial.println(" °C");
+  
+  lcd.setCursor(0, 0);                       // set the cursor to the first column, first row
+  lcd.print("Temp: ");                       // print the temperature label
+  lcd.print(temperature);                    // print the temperature value
+  lcd.print(" C");  
+
+  lcd.setCursor(0, 1);                       // set the cursor to the first column, first row
+  lcd.print("Humidity: ");                       // print the temperature label
+  lcd.print(humidity);                    // print the temperature value
+  lcd.print(" C"); 
+  delay(1000);
 }
+
 
